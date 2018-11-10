21a54d4bd1b3c06415a709d4b7d4b2ff661cb147
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2018-11-10 16:50:17 +0100

    Added features
    The program can now add punctuation, linechanges and a headline

--- write-novel.c
+++ write-novel.c
@@ -10,10 +10,10 @@
 # include <unistd.h>
 # include <string.h>
 # include <time.h>
+# include <ctype.h>
 
 # define WORD_COUNT 3000
 # define WORD_LEN 60
-# define NOVEL_LENGTH 50000
 
 typedef struct {
   char string[WORD_LEN];
@@ -36,27 +36,77 @@ int main (void) {
    return 0;
 }
 
+/* Writes a novel to a .tex file */
 int novelWriter(word *word_list) {
   FILE *fP;
-  int i = 0;
+  int i = 1;
+  int a, b, c;
+  int WORDS = 50000;
 
-  fP = fopen("novel.txt", "w");
+  /* Opens the novel file */
+  fP = fopen("novel-files/novel.tex", "w");
 
+  /* Checks to see if the file was found */
   if (fP == NULL) {
      perror("Error opening file");
      return -1;
   }
 
-  while (i < 50000) {
-    fprintf(fP, "%s ", word_list[rand() % 3000].string);
+  c = rand() % 3000;
+
+  fprintf(fP, "\\documentclass{article}\n");
+  fprintf(fP, "\\begin{document}\n");
+
+  /* Printes a headline */
+  if (islower(word_list[c].string[0])) {
+
+    word_list[c].string[0] = (char)((int)(word_list[c].string[0] - 32));
+    fprintf(fP, "\\section*{%s}\n", word_list[c].string);
+    word_list[c].string[0] = (char)((int)(word_list[c].string[0] + 32));
+  }
+
+  else {
+    fprintf(fP, "\\section{%s}\n", word_list[c].string);
+  }
+
+
+  /* Prints words to the .tex file */
+  while (i <= WORDS) {
+    a = (rand() % 10) + 10;
+    b = rand() % 3000;
+
+    /* Makes the first letter in the file capitalized */
+    if (i == 1) {
+      word_list[b].string[0] = (char)((int)(word_list[b].string[0] - 32));
+      fprintf(fP, " %s", word_list[b].string);
+      word_list[b].string[0] = (char)((int)(word_list[b].string[0] + 32));
+    }
+
+    /* Inputs linechanges here and there. Makes the first letter after a punctuation capitalized */
+    else if (i % a == 0) {
+      fprintf(fP, ".\\\\\n");
+      if (islower(word_list[b].string[0])) {
+
+        word_list[b].string[0] = (char)((int)(word_list[b].string[0] - 32));
+        fprintf(fP, " %s", word_list[b].string);
+        word_list[b].string[0] = (char)((int)(word_list[b].string[0] + 32));
+      }
+    }
+
+    else {
+      fprintf(fP, " %s", word_list[b].string);
+    }
 
     i++;
   }
-  fclose(fP);
 
+  fprintf(fP, "\n\\end{document}");
+
+  fclose(fP);
   return 0;
 }
 
+/* Fills an array with words */
 int fillWords(word *word_list) {
   FILE *fP;
 
@@ -75,6 +125,7 @@ int fillWords(word *word_list) {
   return 0;
 }
 
+/* Prints a list of words */
 void listPrinter(const word *word_list) {
   for (int i = 0; i < WORD_COUNT; i++) {
     printf("%4d | %s\n", i, word_list[i].string);
